0e9ba81a7d0d87bf3afaaa4fa2a08eb1b51d6355
Christian Alonso <christian.alonso@wizeline.com>
2022-03-04 17:54:13 -0600

    Empezando deliverable1

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <div className='content-header'>
+        <i className="fa-brands fa-earlybirds" />
+        <h1>Buscador</h1>
+        <i className="fa-solid fa-cart-shopping"/>
+
+      </div>
+
+
+
+
+    </div>
+  )
+}
